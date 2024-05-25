a905b760e52138ec54c914e86915d16d394b01a6
Caio Marcelo <caio.marcelo79@gmail.com>
2024-05-25 08:12:12 -0300

    Desenvolvimento dos componentes

--- react_front-end/src/components/Formulario.js
+++ react_front-end/src/components/Formulario.js
@@ -2,7 +2,16 @@ function Formulario(){
 
   return(
     <>
-    <h1>Formulario</h1>
+
+    <form>
+      <input type="text" placeholder="Nome" /> <br/>
+      <input type="text" placeholder="Marca" /> <br/>
+
+      <input type="button" value='Cadastrar' />
+      <input type="button" value='Alterar' />
+      <input type="button" value='Remover' />
+      <input type="button" value='Cancelar' />
+    </form>
 
     </>
   )
--- react_front-end/src/components/Tabela.js
+++ react_front-end/src/components/Tabela.js
@@ -2,7 +2,28 @@ function Tabela(){
 
   return(
     <>
-    <h1>Tabela</h1>
+
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nome</th>
+          <th>Marca</th>
+          <th>Selecionar</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+
+    </table>
+    
     </>
   )
 }
